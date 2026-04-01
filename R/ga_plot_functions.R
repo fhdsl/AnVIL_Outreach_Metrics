bde0b6f955fc9258401d8ea69afe0990c50caeba
avahoffman <avamariehoffman@gmail.com>
2026-04-01 15:27:48 -0400

    Make some output corrections

--- R/ga_plot_functions.R
+++ R/ga_plot_functions.R
@@ -5,30 +5,45 @@ library(ggplot2)
 install.packages('plotly')
 library(plotly)
 
+clean_book_names <- function(in_data) {
+  return(in_data %>%
+           mutate(
+             website = case_when(
+               website == "AnVIL Book: Urban Genomics: Identifying population structure among feral pigeon populations" ~ "AnVIL Book: Urban Genomics",
+               website == "AnVIL Book: Epigenetics on AnVIL: Nature vs Nurture" ~ "AnVIL Book: Epigenetics on AnVIL",
+               website == "AnVIL Book Champions" ~ "AnVIL Champions",
+               TRUE ~ website
+             )
+           ) %>%
+           mutate(website = str_remove(website, "AnVIL Book: ")))
+}
+
+
 # ga_dimensions should be the in_data
 make_session_cumsum_sessions <- function(in_data) {
   clean_dat <-
     in_data %>%
+    clean_book_names() %>%
     mutate(date = as.POSIXct(paste0(year, "-", month, "-", day)),
            month_dt = floor_date(date, "month")) %>%
     group_by(website, month_dt) %>%
     summarize(daily = n()) %>%
     mutate(cumsum = cumsum(daily))
   
-    p <- plot_ly(
-      clean_dat,
-      x = ~ month_dt,
-      y = ~ cumsum,
-      color = ~ website,
-      mode = 'lines',
-      text = ~ paste(website)
-    ) %>% layout(
-      showlegend = FALSE,
-      xaxis = list(title = ''),
-      yaxis = list(title = 'Web sessions (cumulative)')
-    )
-    
-    return(p)
+  p <- plot_ly(
+    clean_dat,
+    x = ~ month_dt,
+    y = ~ cumsum,
+    color = ~ website,
+    mode = 'lines',
+    text = ~ paste(website)
+  ) %>% layout(
+    showlegend = FALSE,
+    xaxis = list(title = ''),
+    yaxis = list(title = 'Web sessions (cumulative)')
+  )
+  
+  return(p)
 }
 
 
@@ -36,6 +51,7 @@ make_session_cumsum_sessions <- function(in_data) {
 make_session_monthly_sessions <- function(in_data) {
   clean_dat <-
     in_data %>%
+    clean_book_names() %>% 
     mutate(date = as.POSIXct(paste0(year, "-", month, "-", day)),
            month_dt = floor_date(date, "month")) %>%
     group_by(website, month_dt) %>%
@@ -62,15 +78,8 @@ make_session_monthly_sessions <- function(in_data) {
 make_book_users_barchart <- function(in_data) {
   clean_dat <-
     in_data %>%
-    select(website, totalUsers) %>%
-    mutate(website = case_when(
-      website == "AnVIL Book: Urban Genomics: Identifying population structure among feral pigeon populations" ~ "AnVIL Book: Urban Genomics",
-      website == "AnVIL Book: Epigenetics on AnVIL: Nature vs Nurture" ~ "AnVIL Book: Epigenetics on AnVIL",
-      website == "AnVIL Book Champions" ~ "AnVIL Champions",
-      TRUE ~ website
-    )) %>% 
-    mutate(website = str_remove(website, "AnVIL Book: ")) %>% 
-  mutate(website = fct_reorder(website, totalUsers, .desc = T))
+    clean_book_names() %>%
+    mutate(website = fct_reorder(website, totalUsers, .desc = T))
   
   p <- plot_ly(
     clean_dat,
